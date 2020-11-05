d29323b2a8ca45487d93050787ae5b87b9565765
nhquang <quangpersie1@gmail.com>
2020-11-04 22:32:12 -0500

    input from user

--- Nasa/ViewModels/BaseViewModel.cs
+++ Nasa/ViewModels/BaseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,16 @@ using System.Threading.Tasks;
 
 namespace Nasa.ViewModels
 {
+    
     class BaseViewModel : INotifyPropertyChanged
     {
+        public BaseViewModel()
+        {
+            Cameras = new ObservableCollection<string>();
+            Cameras.Add("Front Hazard Avoidance Cam");
+            Cameras.Add("Rear Hazard Avoidance Cam");
+            
+        }
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propName)
         {
@@ -24,6 +33,27 @@ namespace Nasa.ViewModels
         {
             get { return photos_; }
             set { photos_ = value; OnPropertyChanged(nameof(photos)); }
+        }
+        private ObservableCollection<string> cameras_;
+
+        public ObservableCollection<string> Cameras
+        {
+            get { return cameras_; }
+            set { cameras_ = value; OnPropertyChanged(nameof(Cameras)); }
+        }
+        private int selectedCamera_;
+
+        public int SelectedCamera
+        {
+            get { return selectedCamera_; }
+            set { selectedCamera_ = value; OnPropertyChanged(nameof(SelectedCamera)); }
+        }
+        private string selectedDate_;
+
+        public string SelectedDate
+        {
+            get { return selectedDate_; }
+            set { selectedDate_ = value; OnPropertyChanged(nameof(SelectedDate)); }
         }
     }
 }
--- Nasa/ViewModels/CuriosityViewModel.cs
+++ Nasa/ViewModels/CuriosityViewModel.cs
@@ -27,20 +27,38 @@ namespace Nasa.ViewModels
             get { return image_; }
             set { image_ = value; OnPropertyChanged(nameof(Image)); }
         }
+
         
 
 
 
-        public CuriosityViewModel()
+        public CuriosityViewModel() : base()
         {
             SeePhotos = new RelayCommand(() => execute(), true);
-            
+            Cameras.Add("Mast Cam");
+            SelectedCamera = 0;
+            SelectedDate = "10/12/2020 12:00:00 AM";
+
         }
         async Task execute()
         {
             var args = new Dictionary<string, string>();
-            args.Add("earth_date", "2020-10-22");
-            args.Add("camera", "FHAZ");
+            var parts = SelectedDate.Split(' ').ToList()[0].Split('/');
+            
+            //args.Add("earth_date", String.Join("/", parts));
+            args.Add("earth_date", $"{parts[2]}-{parts[0]}-{parts[1]}");
+            switch (SelectedCamera)
+            {
+                case 0:
+                    args.Add("camera", "FHAZ");
+                    break;
+                case 1:
+                    args.Add("camera", "RHAZ");
+                    break;
+                case 2:
+                    args.Add("camera", "MAST");
+                    break;
+            }
             args.Add("api_key", Encryption.decryption(ConfigurationSettings.AppSettings["APIkey"].Trim()));
             
             var request = HTTPRequest.createRequest($"{ ConfigurationSettings.AppSettings["root"].Trim() }curiosity/photos", args);
