e93f6f405eddb3b0df38630e04d2e6ff8478b4f7
nhquang <quangpersie1@gmail.com>
2020-11-10 23:41:55 -0500

    Exceptions

--- Nasa/ViewModels/CuriosityViewModel.cs
+++ Nasa/ViewModels/CuriosityViewModel.cs
@@ -23,50 +23,64 @@ namespace Nasa.ViewModels
 
         public CuriosityViewModel() : base()
         {
-            SeePhotos = new RelayCommand(() => execute(), true);
-            Cameras.Add("Mast Cam");
-            SelectedCamera = 0;
-            SelectedDate = "10/12/2020 12:00:00 AM";
-
+            try
+            {
+                SeePhotos = new RelayCommand(() => execute(), true);
+                Cameras.Add("Mast Cam");
+                SelectedCamera = 0;
+                SelectedDate = "10/12/2020 12:00:00 AM";
+            }
+            catch(Exception ex)
+            {
+                Logs logs = new Logs();
+                logs.writeException(ex);
+            }
         }
         async Task execute()
         {
-            Visibility1 = "Hidden";
-            Visibility3 = "Hidden";
-            Photos = new ObservableCollection<string>();
-            var args = new Dictionary<string, string>();
-            var parts = SelectedDate.Split(' ').ToList()[0].Split('/');
-            
-            
-            args.Add("earth_date", $"{parts[2]}-{parts[0]}-{parts[1]}");
-            switch (SelectedCamera)
+            try
             {
-                case 0:
-                    args.Add("camera", "FHAZ");
-                    break;
-                case 1:
-                    args.Add("camera", "RHAZ");
-                    break;
-                case 2:
-                    args.Add("camera", "MAST");
-                    break;
+                Visibility1 = "Hidden";
+                Visibility3 = "Hidden";
+                Photos = new ObservableCollection<string>();
+                var args = new Dictionary<string, string>();
+                var parts = SelectedDate.Split(' ').ToList()[0].Split('/');
+
+
+                args.Add("earth_date", $"{parts[2]}-{parts[0]}-{parts[1]}");
+                switch (SelectedCamera)
+                {
+                    case 0:
+                        args.Add("camera", "FHAZ");
+                        break;
+                    case 1:
+                        args.Add("camera", "RHAZ");
+                        break;
+                    case 2:
+                        args.Add("camera", "MAST");
+                        break;
+                }
+                args.Add("api_key", Encryption.decryption(ConfigurationSettings.AppSettings["APIkey"].Trim()));
+
+                var request = HTTPRequest.createRequest($"{ ConfigurationSettings.AppSettings["root"].Trim() }curiosity/photos", args);
+
+                Visibility2 = "Visible";
+                var data = await HTTPRequest.getData(request);
+
+                var photos = JsonConvert.DeserializeObject<Photos>(data);
+
+
+                if (photos.photos.Count != 0)
+                    Photos = new ObservableCollection<string>(photos.photos.Select(i => i.img_src));
+                else { Visibility3 = "Visible"; }
+
+                Visibility2 = "Hidden";
             }
-            args.Add("api_key", Encryption.decryption(ConfigurationSettings.AppSettings["APIkey"].Trim()));
-            
-            var request = HTTPRequest.createRequest($"{ ConfigurationSettings.AppSettings["root"].Trim() }curiosity/photos", args);
-
-            Visibility2 = "Visible";
-            var data = await HTTPRequest.getData(request);
-            
-            var photos = JsonConvert.DeserializeObject<Photos>(data);
-
-
-            if (photos.photos.Count != 0)
-                Photos = new ObservableCollection<string>(photos.photos.Select(i => i.img_src));
-            else { Visibility3 = "Visible"; }
-
-            Visibility2 = "Hidden";
-            
+            catch(Exception ex)
+            {
+                Logs logs = new Logs();
+                logs.writeException(ex);
+            }
         }
     }
 }
